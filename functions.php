05b8dc57909f9765e3c096402fd6a1fc72d5bcb2
Dominik Fringeli <dominik.fringeli@students.fhnw.ch>
2018-08-20 22:13:50 +0200

    added bootstrap css und js

--- functions.php
+++ functions.php
@@ -120,11 +120,20 @@ add_action( 'widgets_init', 'ktf2021_widgets_init' );
  * Enqueue scripts and styles.
  */
 function ktf2021_scripts() {
+	wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+
 	wp_enqueue_style( 'ktf2021-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'ktf2021-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'ktf2021-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'jQuery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array(). '20180820', true);
+
+	wp_enqueue_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(). '20180820', true);
+
+	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array(). '20180820', true);
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
