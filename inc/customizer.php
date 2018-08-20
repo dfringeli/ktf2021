f325c6ac74268f6727882bcbe1b0b690a06a709c
Dominik Fringeli <dominik.fringeli@students.fhnw.ch>
2018-08-20 21:38:51 +0200

    Init: added _s Default Template

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * ktf2021 Theme Customizer
+ *
+ * @package ktf2021
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function ktf2021_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'ktf2021_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'ktf2021_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'ktf2021_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function ktf2021_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function ktf2021_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function ktf2021_customize_preview_js() {
+	wp_enqueue_script( 'ktf2021-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'ktf2021_customize_preview_js' );
