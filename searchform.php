5988cb7c6dbca42ee50fd45b3d252d362c0d520c
dfringeli <dfringeli@outlook.com>
2018-09-27 09:46:17 +0200

    added search form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<div class="input-group search-container mx-auto">
+  <input type="text" class="form-control" placeholder="Was suchst du?" aria-label="Was suchst du?" aria-describedby="button-addon-search">
+  <div class="input-group-append">
+    <button class="btn" type="button" id="button-addon-search">Suche</button>
+  </div>
+</div>
