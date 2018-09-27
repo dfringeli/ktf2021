2cfcecbd12dd09cbfdcb8b975794529a6d73e77f
dfringeli <dfringeli@outlook.com>
2018-09-27 10:08:47 +0200

    improved search

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
-<div class="input-group search-container mx-auto">
-  <input type="text" class="form-control" placeholder="Was suchst du?" aria-label="Was suchst du?" aria-describedby="button-addon-search">
-  <div class="input-group-append">
-    <button class="btn" type="button" id="button-addon-search">Suche</button>
-  </div>
-</div>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <div class="input-group search-container mx-auto">
+    <input type="text" name="s" class="form-control" placeholder="Was suchst du?" aria-label="Was suchst du?" aria-describedby="button-addon-search">
+    <div class="input-group-append">
+        <button class="btn btn-ktf2021-search" type="submit" id="button-addon-search">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
+    </div>
+</form>
