5a06d4056a556ceda33a1f539af08eaf580c1eed
dfringeli <dfringeli@outlook.com>
2019-01-03 14:59:53 +0100

    created 404 page

--- 404.php
+++ 404.php
@@ -14,41 +14,21 @@ get_header();
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ktf2021' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="ktf2021-container-white">
+					<div class="ktf2021-content ktf2021-reveal">
+						<h1 class="page-title text-center" style="margin-bottom: 0"><?php esc_html_e( 'Uups! Jetzt bisch vom Pfad ab choo', 'ktf2021' ); ?></h1>
+					</div>
+				</div><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ktf2021' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ktf2021' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$ktf2021_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ktf2021' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ktf2021_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<div class="ktf2021-container-white">
+					<div class="ktf2021-content ktf2021-reveal">
+						<h2 class="text-center"><?php esc_html_e( 'Hesch öppis bestimmt gsuecht? Versuechs do mit dr Suechi', 'ktf2021' ); ?></h2>
+						<div class="d-flex justify-content-center">
+							<div class="search-container">
+								<?php get_search_form(); ?>
+							</div>
+						</div>
+					</div>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="input-group">
-    <input type="text" name="s" class="form-control" placeholder="Was suchst du?" aria-label="Was suchst du?" aria-describedby="button-addon-search">
+    <input type="text" name="s" class="form-control" placeholder="Was suechsch?" aria-label="Was suchst du?" aria-describedby="button-addon-search">
     <div class="input-group-append">
         <button class="btn btn-ktf2021-search" type="submit" id="button-addon-search">
             <i class="fas fa-search"></i>
