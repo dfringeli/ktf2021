6e6a7f9848ba4b02c0928a282fd8c9766d4b0385
dfringeli <dfringeli@outlook.com>
2019-01-05 11:11:24 +0100

    removed list of tag for posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -56,16 +56,6 @@ if ( ! function_exists( 'ktf2021_entry_footer' ) ) :
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
 	function ktf2021_entry_footer() {
-		// Hide category and tag text for pages.
-		if ( 'post' === get_post_type() ) {
-
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'ktf2021' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'ktf2021' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-			}
-		}
 
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
