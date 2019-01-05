a0ea140e738f51f37e351fa3b00d406c5cd41144
dfringeli <dfringeli@outlook.com>
2019-01-05 12:14:34 +0100

    fixed favicon linking

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -39,11 +39,11 @@ if ( ! function_exists( 'ktf2021_header_style' ) ) :
 
 		?>
 <!-- Favicon -->
-<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
-<link rel="manifest" href="./site.webmanifest">
-<link rel="mask-icon" href="./safari-pinned-tab.svg" color="#a3d01e">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_site_url(); ?>/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_site_url(); ?>/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_site_url(); ?>/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_site_url(); ?>/site.webmanifest">
+<link rel="mask-icon" href="<?php echo get_site_url(); ?>/safari-pinned-tab.svg" color="#a3d01e">
 <meta name="msapplication-TileColor" content="#2d89ef">
 		<?php
 		
