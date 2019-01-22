334fdf0992489520ac30c5da8ef9d5e5d1a76164
dfringeli <dfringeli@outlook.com>
2019-01-22 16:18:12 +0100

    moved favicon import to header because of doctype error

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -10,10 +10,3 @@
  * @package ktf2021
  */
 ?>
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_site_url(); ?>/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_site_url(); ?>/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_site_url(); ?>/favicon-16x16.png">
-<link rel="manifest" href="<?php echo get_site_url(); ?>/site.webmanifest">
-<link rel="mask-icon" href="<?php echo get_site_url(); ?>/safari-pinned-tab.svg" color="#a3d01e">
-<meta name="msapplication-TileColor" content="#2d89ef">
-<meta name="theme-color" content="<?php echo get_theme_mod('google_chrome_theme'); ?>">
