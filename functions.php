ca51ed9557fda142add87f665f09131e162febcc
dfringeli <dfringeli@outlook.com>
2019-01-22 11:11:48 +0100

    removed theme support: rss, background, logo, sidebar, header added header, footer menu positions

--- functions.php
+++ functions.php
@@ -24,9 +24,6 @@ if ( ! function_exists( 'ktf2021_setup' ) ) :
 		 */
 		load_theme_textdomain( 'ktf2021', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -44,7 +41,8 @@ if ( ! function_exists( 'ktf2021_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'ktf2021' ),
+			'Header' => esc_html__( 'Header', 'ktf2021' ),
+			'Footer' => esc_html__( 'Footer', 'ktf2021' ),
 		) );
 
 		/*
@@ -57,27 +55,6 @@ if ( ! function_exists( 'ktf2021_setup' ) ) :
 			'comment-list',
 			'gallery',
 			'caption',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'ktf2021_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
 		) );
 	}
 endif;
@@ -97,24 +74,6 @@ function ktf2021_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'ktf2021_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'ktf2021_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function ktf2021_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'ktf2021' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'ktf2021' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'ktf2021_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -4,86 +4,16 @@
  *
  * You can add an optional custom header image to header.php like so ...
  *
-	<?php the_header_image_tag(); ?>
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
  * @package ktf2021
  */
-
-/**
- * Set up the WordPress core custom header feature.
- *
- * @uses ktf2021_header_style()
- */
-function ktf2021_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'ktf2021_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'ktf2021_header_style',
-	) ) );
-}
-add_action( 'after_setup_theme', 'ktf2021_custom_header_setup' );
-
-if ( ! function_exists( 'ktf2021_header_style' ) ) :
-	/**
-	 * Styles the header image and text displayed on the blog.
-	 *
-	 * @see ktf2021_custom_header_setup().
-	 */
-	function ktf2021_header_style() {
-		$header_text_color = get_header_textcolor();
-
-		?>
-<!-- Favicon -->
+?>
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_site_url(); ?>/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_site_url(); ?>/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_site_url(); ?>/favicon-16x16.png">
 <link rel="manifest" href="<?php echo get_site_url(); ?>/site.webmanifest">
 <link rel="mask-icon" href="<?php echo get_site_url(); ?>/safari-pinned-tab.svg" color="#a3d01e">
 <meta name="msapplication-TileColor" content="#2d89ef">
-		<?php
-		
-		/**
-		 * Google Chrome Theme
-		 */
-		?>
-		<meta name="theme-color" content="<?php echo get_theme_mod('google_chrome_theme'); ?>">
-		<?php
-
-		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-			?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-			}
-		<?php
-		// If the user has set a custom color for the text use that.
-		else :
-			?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
-		</style>
-		<?php
-	}
-endif;
+<meta name="theme-color" content="<?php echo get_theme_mod('google_chrome_theme'); ?>">
