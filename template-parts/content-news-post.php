99ef4f90d6a2aaf1c78f3c8b07dea89222807177
dfringeli <dfringeli@outlook.com>
2019-04-17 07:46:22 +0200

    improved responsive of posts

--- template-parts/content-news-post.php
+++ template-parts/content-news-post.php
@@ -9,20 +9,18 @@
 
 ?>
 
-<article class="ktf2021-post d-flex flex-column" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="ktf2021-post-image">
-		<?php
-			// Get the post thumbnail
-			$post_thumb_id = get_post_thumbnail_id( $post_id );
-			if ($post_thumb_id) {
-				echo sprintf(
-					'<div class="ktf2021-post-image"><a href="%1$s"><img src="%2$s" /></a></div>',
-					esc_url( get_permalink( $post_id ) ),
-					wp_get_attachment_url( $post_thumb_id, array( '', '200' ) )
-				);
-			}
-		?>
-	</div>
+<article class="ktf2021-post post-max-width d-flex flex-column" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Get the post thumbnail
+		$post_thumb_id = get_post_thumbnail_id( $post );
+		if ($post_thumb_id) {
+			echo sprintf(
+				'<a class="ktf2021-post-image-link" href="%1$s"><div class="ktf2021-post-image" style="background-image: url(%2$s)"></div></a>',
+				esc_url( get_permalink( $post ) ),
+				wp_get_attachment_url( $post_thumb_id, array( '', '200' ) )
+			);
+		}
+	?>
 	<div class="ktf2021-post-title">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</div>
